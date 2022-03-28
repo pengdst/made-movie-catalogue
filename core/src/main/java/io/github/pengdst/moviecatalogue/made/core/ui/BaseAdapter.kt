88655ca26d66e63556e53712c8d99b83ed4f8b57
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2022-03-28 16:30:58 +0700

    create base adapter

---
+++ core/src/main/java/io/github/pengdst/moviecatalogue/made/core/ui/BaseAdapter.kt
@@ -0,0 +1,36 @@
+package io.github.pengdst.moviecatalogue.made.core.ui
+
+import android.view.View
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseAdapter {
+
+    interface Callback {
+        interface OnItemClick<Data> {
+            fun onItemClick(view: View, data: Data, position: Int)
+        }
+    }
+
+    abstract class Recycler {
+    }
+
+    abstract class ListAnimated<Data, VH : RecyclerView.ViewHolder>(
+        diffItemCallback: DiffUtil.ItemCallback<Data>
+    ) : ListAdapter<Data, VH>(diffItemCallback) {
+
+        protected var itemClickCallback: Callback.OnItemClick<Data>? = null
+
+        fun submitData(list: List<Data>) = submitList(list)
+
+        fun setOnItemClickListener(onClick: (View, Data, Int) -> Unit) {
+            itemClickCallback = object : Callback.OnItemClick<Data> {
+                override fun onItemClick(view: View, data: Data, position: Int) {
+                    onClick(view, data, position)
+                }
+            }
+        }
+    }
+
+}
