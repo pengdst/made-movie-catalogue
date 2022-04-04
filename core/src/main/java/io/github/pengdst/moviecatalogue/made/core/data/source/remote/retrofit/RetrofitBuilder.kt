d613554af4f39b44a5011e96d1d4a58fc2f0b862
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2022-04-04 12:36:45 +0700

    add ssl pinning

--- core/src/main/java/io/github/pengdst/moviecatalogue/made/core/data/source/remote/retrofit/RetrofitBuilder.kt
+++ core/src/main/java/io/github/pengdst/moviecatalogue/made/core/data/source/remote/retrofit/RetrofitBuilder.kt
@@ -1,6 +1,7 @@
 package io.github.pengdst.moviecatalogue.made.core.data.source.remote.retrofit
 
 import io.github.pengdst.moviecatalogue.made.core.BuildConfig
+import okhttp3.CertificatePinner
 import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -18,7 +19,14 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitBuilder {
 
     private fun okHttpClient(): OkHttpClient {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/oD/WAoRPvbez1Y2dfYfuo4yujAcYHXdv1Ivb2v2MOKk=")
+            .add(hostname, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .build()
         return OkHttpClient.Builder()
+            .certificatePinner(certificatePinner)
             .addInterceptor { chain ->
                 val original: Request = chain.request()
                 val originalHttpUrl: HttpUrl = original.url
